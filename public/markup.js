526f1c20d93c80b036ba686f1a3edb8f815b254b
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-06 17:30:35 +0300

    Add the `Tag` class

--- public/markup.js
+++ public/markup.js
@@ -9,3 +9,38 @@ class Text {
     return document.createTextNode(this.text);
   }
 }
+
+class Tag {
+  name;
+  attributes = {};
+  children = [];
+
+  constructor(name, attributes, children) {
+    if (name === undefined) {
+      throw new Error("tag name is required");
+    }
+    // support for a call with only two arguments: a name and children
+    if (children === undefined && Array.isArray(attributes)) {
+      children = attributes;
+      attributes = undefined;
+    }
+
+    this.name = name;
+    this.attributes = attributes ?? this.attributes;
+    this.children = children ?? this.children;
+  }
+
+  toDOM() {
+    const element = document.createElement(this.name);
+
+    Object.entries(this.attributes).forEach(([name, value]) => {
+      element.setAttribute(name, value);
+    });
+
+    this.children.forEach((child) => {
+      element.appendChild(child.toDOM());
+    });
+
+    return element;
+  }
+}
