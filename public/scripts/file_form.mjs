7b1958f1f392984f4896df3b94be9e72aa28dd10
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-16 00:38:23 +0300

    Add a toast on successful uploading

--- public/scripts/file_form.mjs
+++ public/scripts/file_form.mjs
@@ -1,4 +1,7 @@
 import * as api from "./libs/api.mjs";
+import { showToast } from "./show_toast.mjs";
+import { defaultToastKinds } from "./components/ToastKind.mjs";
+import { Text, Tag } from "./libs/markup.mjs";
 import { updateFileList } from "./update_file_list.mjs";
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -7,6 +10,12 @@ window.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
 
     await api.saveFile(new FormData(fileForm));
+    showToast(defaultToastKinds.info, [
+      new Text("The "),
+      new Tag("code", [new Text(fileForm.elements.file.files[0].name)]),
+      new Text(" file was successfully uploaded."),
+    ]);
+
     await updateFileList();
   });
 
