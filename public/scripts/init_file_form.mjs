835df5015ce2184e14fd00273c06e4a0e52f7754
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-18 02:37:14 +0300

    Add a toast on any error

--- public/scripts/init_file_form.mjs
+++ public/scripts/init_file_form.mjs
@@ -1,3 +1,4 @@
+import { withErrorDisplaying } from "./show_toast.mjs";
 import * as api from "./libs/api.mjs";
 import { showToast } from "./show_toast.mjs";
 import { defaultToastKinds } from "./components/ToastKind.mjs";
@@ -7,22 +8,26 @@ import { updateFileList } from "./update_file_list.mjs";
 export function initFileForm() {
   const fileForm = document.querySelector(".file-form");
   fileForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
+    await withErrorDisplaying(async () => {
+      event.preventDefault();
 
-    const fileInput = fileForm.elements.file;
-    await api.saveFile(new FormData(fileForm));
-    showToast(defaultToastKinds.info, [
-      new Text("The "),
-      new Tag("code", [new Text(fileInput.files[0].name)]),
-      new Text(" file was successfully uploaded."),
-    ]);
+      const fileInput = fileForm.elements.file;
+      await api.saveFile(new FormData(fileForm));
+      showToast(defaultToastKinds.info, [
+        new Text("The "),
+        new Tag("code", [new Text(fileInput.files[0].name)]),
+        new Text(" file was successfully uploaded."),
+      ]);
 
-    await updateFileList();
+      await updateFileList();
+    });
   });
 
   const totalDeleteButton = document.querySelector(".total-delete-button");
   totalDeleteButton.addEventListener("click", async () => {
-    await api.deleteFiles();
-    await updateFileList();
+    await withErrorDisplaying(async () => {
+      await api.deleteFiles();
+      await updateFileList();
+    });
   });
 }
--- public/scripts/update_file_list.mjs
+++ public/scripts/update_file_list.mjs
@@ -1,23 +1,28 @@
+import { withErrorDisplaying } from "./show_toast.mjs";
 import * as api from "./libs/api.mjs";
 import { Tag, removeAllChildren } from "./libs/markup.mjs";
 import { FileInfoView } from "./components/FileInfoView.mjs";
 
 export async function updateFileList() {
-  const fileInfos = await api.getFiles();
+  await withErrorDisplaying(async () => {
+    const fileInfos = await api.getFiles();
 
-  const fileListView = document.querySelector(".file-list");
-  removeAllChildren(fileListView);
+    const fileListView = document.querySelector(".file-list");
+    removeAllChildren(fileListView);
 
-  fileInfos.forEach((fileInfo) => {
-    const fileInfoView = new Tag("li", [
-      FileInfoView({
-        fileInfo,
-        onFileDeleting: async () => {
-          await api.deleteFile(fileInfo.Name);
-          await updateFileList();
-        },
-      }),
-    ]);
-    fileListView.appendChild(fileInfoView.toDOM());
+    fileInfos.forEach((fileInfo) => {
+      const fileInfoView = new Tag("li", [
+        FileInfoView({
+          fileInfo,
+          onFileDeleting: async () => {
+            await withErrorDisplaying(async () => {
+              await api.deleteFile(fileInfo.Name);
+              await updateFileList();
+            });
+          },
+        }),
+      ]);
+      fileListView.appendChild(fileInfoView.toDOM());
+    });
   });
 }
