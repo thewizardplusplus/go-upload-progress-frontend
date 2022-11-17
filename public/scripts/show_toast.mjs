9c1c246521cd66c53d7c9c4ecffb542f3a47eb22
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-18 02:36:15 +0300

    Add the `withErrorDisplaying()` function

--- public/scripts/show_toast.mjs
+++ public/scripts/show_toast.mjs
@@ -1,4 +1,5 @@
 import { Toast, defaultToastLifetimeInMs } from "./components/Toast.mjs";
+import { defaultToastKinds } from "./components/ToastKind.mjs";
 
 let globalToastCount = 0;
 
@@ -8,4 +9,17 @@ export function showToast(kind, body, lifetimeInMs = defaultToastLifetimeInMs) {
 
   const toastContainer = document.querySelector(".toast-container");
   toastContainer.appendChild(toast.toDOM());
+}
+
+export async function withErrorDisplaying(baseHandler) {
+  try {
+    await baseHandler();
+  } catch (err) {
+    const trimmedErrMessage = err.message.trim();
+    const errMessageAsSentence =
+      trimmedErrMessage.at(0).toUpperCase() +
+      trimmedErrMessage.slice(1) +
+      (!trimmedErrMessage.endsWith(".") ? "." : "");
+    showToast(defaultToastKinds.error, errMessageAsSentence);
+  }
 }
