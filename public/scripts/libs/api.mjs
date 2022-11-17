a410afc2753dbf0553eeabd5aff746f3b53ce01a
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-17 23:00:00 +0300

    Improve error handling in the API client functions

--- public/scripts/libs/api.mjs
+++ public/scripts/libs/api.mjs
@@ -2,16 +2,28 @@ const fileAPIRoute = "/api/v1/files";
 
 export async function getFiles() {
   const response = await fetch(fileAPIRoute);
+  await checkResponse(response);
+
   return await response.json();
 }
 
 export async function saveFile(formData) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
+    const rejectWithError = (errMessage) => {
+      reject(new Error(errMessage));
+    };
+
     const request = new XMLHttpRequest();
     request.addEventListener("load", () => {
-      if (request.status == 201) {
-        resolve();
+      if (request.status < 200 || request.status > 299) {
+        rejectWithError(request.responseText);
+        return;
       }
+
+      resolve();
+    });
+    request.addEventListener("error", () => {
+      rejectWithError("network error");
     });
 
     request.open("POST", fileAPIRoute);
@@ -21,9 +33,22 @@ export async function saveFile(formData) {
 
 export async function deleteFile(filename) {
   const params = new URLSearchParams({ filename });
-  await fetch(`${fileAPIRoute}?${params}`, { method: "DELETE" });
+  const response = await fetch(`${fileAPIRoute}?${params}`, {
+    method: "DELETE",
+  });
+  await checkResponse(response);
 }
 
 export async function deleteFiles() {
-  await fetch(fileAPIRoute, { method: "DELETE" });
+  const response = await fetch(fileAPIRoute, { method: "DELETE" });
+  await checkResponse(response);
+}
+
+async function checkResponse(response) {
+  if (response.ok) {
+    return;
+  }
+
+  const errMessage = await response.text();
+  throw new Error(errMessage);
 }
