620f9a5c07b814814f1ca11f54ed38c464a03ac1
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-18 03:03:26 +0300

    Add the `startFileListUpdating()` function

--- public/scripts/update_file_list.mjs
+++ public/scripts/update_file_list.mjs
@@ -3,6 +3,8 @@ import * as api from "./libs/api.mjs";
 import { Tag, removeAllChildren } from "./libs/markup.mjs";
 import { FileInfoView } from "./components/FileInfoView.mjs";
 
+const defaultFileListUpdatingTimeout = 1000;
+
 export async function updateFileList() {
   await withErrorDisplaying(async () => {
     const fileInfos = await api.getFiles();
@@ -25,4 +27,20 @@ export async function updateFileList() {
       fileListView.appendChild(fileInfoView.toDOM());
     });
   });
+}
+
+export async function startFileListUpdating() {
+  while (true) {
+    const startUnixTimeInMs = new Date().getTime();
+    await updateFileList();
+
+    const elapsedTimeInMs = new Date().getTime() - startUnixTimeInMs;
+    const fileListUpdatingTimeout = Math.max(
+      defaultFileListUpdatingTimeout - elapsedTimeInMs,
+      0
+    );
+    await new Promise((resolve) => {
+      setTimeout(resolve, fileListUpdatingTimeout);
+    });
+  }
 }
