d7a895c5f745f5f28ed5d48478870fb115699b9a
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-19 04:11:58 +0300

    Add the `ProgressView` class

---
+++ public/scripts/components/ProgressView.mjs
@@ -0,0 +1,28 @@
+const showingClass = "d-block";
+const hidingClass = "d-none";
+
+export class ProgressView {
+  #container;
+  #bar;
+
+  constructor() {
+    this.#container = document.querySelector(".progress-container");
+    this.#bar = document.querySelector(".progress-bar");
+  }
+
+  show() {
+    this.#container.classList.replace(hidingClass, showingClass);
+  }
+
+  hide() {
+    this.#container.classList.replace(showingClass, hidingClass);
+  }
+
+  setProgress(progressRatio) {
+    const progressInPercent = Math.round(progressRatio * 100);
+    const formattedProgressInPercent = `${progressInPercent}%`;
+
+    this.#bar.style.width = formattedProgressInPercent;
+    this.#bar.innerText = formattedProgressInPercent;
+  }
+}
--- public/scripts/init_file_form.mjs
+++ public/scripts/init_file_form.mjs
@@ -1,3 +1,4 @@
+import { ProgressView } from "./components/ProgressView.mjs";
 import { withErrorDisplaying } from "./show_toast.mjs";
 import * as api from "./libs/api.mjs";
 import { showToast } from "./show_toast.mjs";
@@ -6,24 +7,20 @@ import { Text, Tag } from "./libs/markup.mjs";
 import { updateFileList } from "./update_file_list.mjs";
 
 export function initFileForm() {
-  const progressContainer = document.querySelector(".progress-container");
-  const progressBar = document.querySelector(".progress-bar");
+  const progressView = new ProgressView();
   const fileForm = document.querySelector(".file-form");
   fileForm.addEventListener("submit", async (event) => {
     await withErrorDisplaying(async () => {
       event.preventDefault();
 
-      progressContainer.classList.replace("d-none", "d-block");
-      progressBar.style.width = "0";
-      progressBar.innerText = "0%";
+      progressView.show();
 
+      progressView.setProgress(0);
       await api.saveFile(new FormData(fileForm), ({ loaded, total }) => {
-        const progressInPercent = Math.round((loaded / total) * 100);
-        progressBar.style.width = `${progressInPercent}%`;
-        progressBar.innerText = `${progressInPercent}%`;
+        progressView.setProgress(loaded / total);
       });
 
-      progressContainer.classList.replace("d-block", "d-none");
+      progressView.hide();
 
       const fileInput = fileForm.elements.file;
       showToast(defaultToastKinds.info, [
