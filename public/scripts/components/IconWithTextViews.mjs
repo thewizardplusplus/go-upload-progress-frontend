a2fbd4e7bbf5c6e47616ab3c58ba0b32574850e0
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-25 23:00:00 +0300

    Document the `IconWithTextViews` component

--- public/scripts/components/IconWithTextViews.mjs
+++ public/scripts/components/IconWithTextViews.mjs
@@ -1,6 +1,26 @@
+/**
+ * @module components/IconWithTextViews
+ */
+
 import { Text, transformToChildren } from '../libs/markup.mjs'
 import { IconView } from './IconView.mjs'
 
+/**
+ * @typedef {import('../libs/markup.mjs').Node} Node
+ */
+
+/**
+ * @typedef {Object} IconWithTextViewsAttributes
+ * @property {string} iconName
+ * @property {string} [iconColorStyle]
+ * @property {string|Node|Array.<Node>} text
+ */
+
+/**
+ * @function
+ * @param {IconWithTextViewsAttributes} attributes
+ * @returns {Array.<Node>}
+ */
 export function IconWithTextViews(attributes) {
   return [
     IconView({ name: attributes.iconName, colorStyle: attributes.iconColorStyle }),
