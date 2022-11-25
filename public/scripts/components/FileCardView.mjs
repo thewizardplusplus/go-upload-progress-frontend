b8bbd882877f56551692da9502d5e0207114a791
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-25 23:00:00 +0300

    Document the `FileCardView` component

--- public/scripts/components/FileCardView.mjs
+++ public/scripts/components/FileCardView.mjs
@@ -1,13 +1,41 @@
+/**
+ * @module components/FileCardView
+ */
+
 import { Tag } from '../libs/markup.mjs'
 import { FileInfoView } from './FileInfoView.mjs'
 import { IconView } from './IconView.mjs'
 
 const disabledIDCharacters = /[^A-Za-z\d]+/g
 
+/**
+ * @function
+ * @param {string} filename
+ * @returns {string}
+ */
 export function makeFileCardID(filename) {
   return `file-card-${filename.replaceAll(disabledIDCharacters, '-')}`
 }
 
+/**
+ * @typedef {import('../libs/api.mjs').FileInfo} FileInfo
+ */
+
+/**
+ * @typedef {import('../libs/markup.mjs').EventHandler} EventHandler
+ */
+
+/**
+ * @typedef {Object} FileCardViewAttributes
+ * @property {FileInfo} fileInfo
+ * @property {EventHandler} onFileDeleting
+ */
+
+/**
+ * @function
+ * @param {FileCardViewAttributes} attributes
+ * @returns {Tag}
+ */
 export function FileCardView(attributes) {
   const fileCardID = makeFileCardID(attributes.fileInfo.Name)
   return new Tag('div', { id: fileCardID, class: 'card mb-3' }, [
