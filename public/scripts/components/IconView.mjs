ba637209ce33d66fb6ad4d01f630b4581de271bb
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-25 23:00:00 +0300

    Document the `IconView` component

--- public/scripts/components/IconView.mjs
+++ public/scripts/components/IconView.mjs
@@ -1,5 +1,20 @@
+/**
+ * @module components/IconView
+ */
+
 import { Tag } from '../libs/markup.mjs'
 
+/**
+ * @typedef {Object} IconViewAttributes
+ * @property {string} name
+ * @property {string} [colorStyle]
+ */
+
+/**
+ * @function
+ * @param {IconViewAttributes} attributes
+ * @returns {Tag}
+ */
 export function IconView(attributes) {
   const colorStyleClass = attributes.colorStyle ? 'text-' + attributes.colorStyle : ''
   return new Tag('i', { class: `bi-${attributes.name} ${colorStyleClass}` })
