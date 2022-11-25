cc55fd7ec81abb61e3b9c83a29288015eda48419
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-25 23:00:00 +0300

    Document the `NoFilesView` component

--- public/scripts/components/NoFilesView.mjs
+++ public/scripts/components/NoFilesView.mjs
@@ -1,8 +1,20 @@
+/**
+ * @module components/NoFilesView
+ */
+
 import { Tag } from '../libs/markup.mjs'
 import { IconWithTextViews } from './IconWithTextViews.mjs'
 
+/**
+ * @constant
+ * @type {string}
+ */
 export const noFilesID = 'no-files'
 
+/**
+ * @function
+ * @returns {Tag}
+ */
 export function NoFilesView() {
   return new Tag('p', { id: noFilesID, class: 'text-muted' }, [
     ...IconWithTextViews({ iconName: 'file-earmark-x', text: 'No files' }),
