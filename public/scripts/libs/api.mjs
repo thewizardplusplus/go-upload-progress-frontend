98a402405a94609eddbe0ad8b9426ab035039306
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-25 23:00:00 +0300

    Document the `api` module

--- public/scripts/libs/api.mjs
+++ public/scripts/libs/api.mjs
@@ -1,5 +1,33 @@
+/**
+ * @module libs/api
+ */
+
 const fileAPIRoute = '/api/v1/files'
 
+/**
+ * @typedef {Object} FileInfo
+ * @property {string} Name
+ * @property {number} SizeInB
+ * @property {string} ModificationTime In RFC 3339 format, e.g. "2006-01-02T15:04:05.123456789+03:00"
+ */
+
+/**
+ * @typedef {Object} ProgressEventHandlerParams
+ * @property {number} loaded
+ * @property {number} total
+ */
+
+/**
+ * @callback ProgressEventHandler
+ * @param {ProgressEventHandlerParams} params
+ * @returns {void}
+ */
+
+/**
+ * @function
+ * @async
+ * @returns {Promise.<Array.<FileInfo>>}
+ */
 export async function getFiles() {
   const response = await fetch(fileAPIRoute)
   await throwOnUnsuccessfulResponse(response)
@@ -7,9 +35,16 @@ export async function getFiles() {
   return await response.json()
 }
 
+/**
+ * @function
+ * @async
+ * @param {FormData} formData
+ * @param {ProgressEventHandler} [progressEventHandler]
+ * @returns {Promise.<FileInfo>}
+ */
 export async function saveFile(formData, progressEventHandler) {
   return new Promise((resolve, reject) => {
-    const rejectWithError = errMessage => {
+    const rejectWithError = (/** @type {string} */ errMessage) => {
       reject(new Error(errMessage))
     }
 
@@ -40,18 +75,29 @@ export async function saveFile(formData, progressEventHandler) {
   })
 }
 
+/**
+ * @function
+ * @async
+ * @param {string} filename
+ * @returns {Promise.<void>}
+ */
 export async function deleteFile(filename) {
   const params = new URLSearchParams({ filename })
   const response = await fetch(`${fileAPIRoute}?${params}`, { method: 'DELETE' })
   await throwOnUnsuccessfulResponse(response)
 }
 
+/**
+ * @function
+ * @async
+ * @returns {Promise.<void>}
+ */
 export async function deleteFiles() {
   const response = await fetch(fileAPIRoute, { method: 'DELETE' })
   await throwOnUnsuccessfulResponse(response)
 }
 
-async function throwOnUnsuccessfulResponse(response) {
+async function throwOnUnsuccessfulResponse(/** @type {Response} */ response) {
   if (response.ok) {
     return
   }
