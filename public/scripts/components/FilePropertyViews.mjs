262fe4c6dde3b90f1a256045cbf2bfd747a4d9cd
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-25 23:00:00 +0300

    Document the `FilePropertyViews` component

--- public/scripts/components/FilePropertyViews.mjs
+++ public/scripts/components/FilePropertyViews.mjs
@@ -1,8 +1,30 @@
+/**
+ * @module components/FilePropertyViews
+ */
+
 import { Text, Tag, capitalizeFirstLetter } from '../libs/markup.mjs'
 import { IconWithTextViews } from './IconWithTextViews.mjs'
 
+/**
+ * @typedef {import('../libs/markup.mjs').Node} Node
+ */
+
+/**
+ * @typedef {Object} FilePropertyViewsAttributes
+ * @property {string} name
+ * @property {string} valueIconName
+ * @property {string|Node|Array.<Node>} valueText
+ * @property {boolean} [isLast]
+ */
+
+/**
+ * @function
+ * @param {FilePropertyViewsAttributes} attributes
+ * @returns {Array.<Node>}
+ */
 export function FilePropertyViews(attributes) {
   return [
+    // @ts-ignore
     new Tag('dt', [new Text(formatName(attributes.name))]),
     new Tag('dd', attributes.isLast ? { class: 'mb-0' } : undefined, [
       ...IconWithTextViews({ iconName: attributes.valueIconName, text: attributes.valueText }),
@@ -10,6 +32,6 @@ export function FilePropertyViews(attributes) {
   ]
 }
 
-function formatName(name) {
+function formatName(/** @type {string} */ name) {
   return capitalizeFirstLetter(name) + ':'
 }
