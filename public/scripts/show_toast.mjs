442307b84a31f0d355674bba3815f700ee3b1e85
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-27 13:54:21 +0300

    Document the `show_toast` module

--- public/scripts/show_toast.mjs
+++ public/scripts/show_toast.mjs
@@ -1,25 +1,54 @@
+/**
+ * @module show_toast
+ */
+
 import { ToastKind, ToastView, defaultToastLifetimeInMs } from './components/ToastView.mjs'
 import { capitalizeFirstLetter } from './libs/markup.mjs'
 
+/**
+ * @typedef {import('./libs/markup.mjs').Node} Node
+ */
+
+/**
+ * @callback BaseHandler
+ * @async
+ * @returns {Promise.<void>}
+ */
+
 let globalToastCount = 0
 
+/**
+ * @function
+ * @param {ToastKind} kind
+ * @param {string|Node|Array.<Node>} body
+ * @param {number} [lifetimeInMs=defaultToastLifetimeInMs]
+ * @returns {void}
+ */
 export function showToast(kind, body, lifetimeInMs = defaultToastLifetimeInMs) {
   const toastID = globalToastCount++
   const toastView = ToastView({ id: toastID, kind, body, lifetimeInMs })
 
   const toastContainer = document.querySelector('.toast-container')
+  // @ts-ignore
   toastContainer.appendChild(toastView.toDOM())
 }
 
+/**
+ * @function
+ * @async
+ * @param {BaseHandler} baseHandler
+ * @returns {Promise.<void>}
+ */
 export async function withErrorDisplaying(baseHandler) {
   try {
     await baseHandler()
   } catch (err) {
+    // @ts-ignore
     showToast(ToastKind.error, formatErrMessage(err.message))
   }
 }
 
-function formatErrMessage(errMessage) {
+function formatErrMessage(/** @type {string} */ errMessage) {
   const trimmedErrMessage = errMessage.trim()
   return capitalizeFirstLetter(trimmedErrMessage) + (!trimmedErrMessage.endsWith('.') ? '.' : '')
 }
